e0785491042d336065b24abcba0c930232a89b91
Александр Луцик <sasha01.lucik@gmail.com>
2020-04-30 14:23:21 +1000

    Added SelectionSort2 class and TestSelectionSort2 class

--- src/main/java/ru/fefupyjava/algodsjava/sort/SelectionSort2.java
+++ src/main/java/ru/fefupyjava/algodsjava/sort/SelectionSort2.java
@@ -4,62 +4,35 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class SelectionSort2 extends Sort {
-<<<<<<< HEAD
-
-    public SelectionSort2(ArrayList<Integer> arrayList) {
-        super();
-    }
-
     @Override
     public List doSort(ArrayList<Integer> arrayList) {
 
-            int i, j;
-            int min, temp;
+        int i, j;
+        int min, temp;
 
-            for (i = 0; i < arrayList.size() - 1; i++) {
+        for (i = 0; i < arrayList.size() - 1; i++) {
 
-                min = i;
+            min = i;
 
-                for (j = i + 1; j < arrayList.size(); j++) {
+            for (j = i + 1; j < arrayList.size(); j++) {
 
-                    if (arrayList.get(j) < arrayList.get(min))
-                        min = j;
-
-                }
-
-                if (min != i) {
-
-                    temp = arrayList.get(i);
-                    arrayList.set(i, arrayList.get(min));
-                    arrayList.set(min, temp);
-
-                }
+                if (arrayList.get(j) < arrayList.get(min))
+                    min = j;
 
             }
 
-            return arrayList;
+            if (min != i) {
 
-        }
+                temp = arrayList.get(i);
+                arrayList.set(i, arrayList.get(min));
+                arrayList.set(min, temp);
 
-    }
-=======
-    @Override
-    public List doSort(ArrayList<Integer> arrayList) {
-        for (int i = 0; i < arrayList.size(); i++) {
-            int min = arrayList.get(i);
-            int minInd = i;
-            for (int j = i + 1; j < arrayList.size(); j++) {
-                if (min > arrayList.get(j)) {
-                    min = arrayList.get(j);
-                    minInd = j;
-                }
             }
-            int temp = arrayList.get(i);
-            arrayList.set(i, arrayList.get(minInd));
-            arrayList.set(minInd, temp);
+
         }
+
         return arrayList;
-    }
-}
 
->>>>>>> a19da8ff89c010020f869a5d91f8f717eb83470e
+    }
+
+}
--- src/test/java/ru/fefupyjava/algodsjava/TestSelectionSort2.java
+++ src/test/java/ru/fefupyjava/algodsjava/TestSelectionSort2.java
@@ -2,9 +2,8 @@ package ru.fefupyjava.algodsjava;
 
 import org.junit.Assert;
 import org.junit.Test;
-<<<<<<< HEAD
-
 import java.util.ArrayList;
+import java.util.Comparator;
 
 import ru.fefupyjava.algodsjava.sort.SelectionSort2;
 import ru.fefupyjava.algodsjava.sort.Sort;
@@ -17,37 +16,12 @@ public class TestSelectionSort2 {
         for (int i = 0; i < 10; i++) {
             arrayList.add((int) (Math.random() * 100));
         }
-        Sort sort = new SelectionSort2(arrayList);
-        sort.doSort(arrayList);
-        for (int i = 0; i < 10; i++) System.out.print("SelectionSort : " + arrayList.get(i) + " ");
-    }
-=======
-import ru.fefupyjava.algodsjava.sort.Sort;
-import SelectionSort2;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.Random;
+        ArrayList<Integer> sorted = (ArrayList<Integer>) arrayList.clone();
+        sorted.sort(Comparator.naturalOrder());
 
-public class TestSelectionSort {
-    @Test
-    public void testQuickSort() throws Exception {
-        ArrayList<Integer> arrayList = new ArrayList<>();
-
-        Random randomInt = new Random();
-
-        for(int i = 0; i < 15; i++) {
-            arrayList.add(randomInt.nextInt(20));
-        }
-
-        ArrayList<Integer> compArray = new ArrayList<Integer>();
-        compArray.addAll(0, arrayList);
-        compArray.sort(Comparator.naturalOrder());
-
-
-        Sort selectionSort = new SelectionSort2();
-        Assert.assertEquals(compArray, selectionSort.doSort(arrayList));
+        Sort sort = new SelectionSort2();
+        Assert.assertEquals(sorted, sort.doSort(arrayList));
     }
 
->>>>>>> a19da8ff89c010020f869a5d91f8f717eb83470e
 }
