1482bdf99144ed73daedb90c29c936b8b80b1671
odnovilEugene <mr.odnovil@mail.ru>
2020-04-30 16:38:55 +1000

    Fixed testFibonacciNumbersMemoized

--- src/test/java/ru/fefupyjava/algodsjava/TestFibonacciNumber.java
+++ src/test/java/ru/fefupyjava/algodsjava/TestFibonacciNumber.java
@@ -38,18 +38,13 @@ public class TestFibonacciNumber {
         Assert.assertEquals(FIBONACCI, algorithmOfFib.run(randomNumberOfFib - 1, "recursive"));
     }
     @Test
-    public void testFibonacciNumbersRecursiveMemoized() throws Exception{
-        Random randomInt = new Random();
-        int randomNumberOfFib = randomInt.nextInt(15);
-        int FIBONACCI = 0;
-        int FIBONACCI1 = 0;
-        int FIBONACCI2 = 1;
-        for(int i = 2; i < randomNumberOfFib; i++){
-            FIBONACCI = FIBONACCI1 + FIBONACCI2;
-            FIBONACCI1 = FIBONACCI2;
-            FIBONACCI2 = FIBONACCI;
-        }
-        Assert.assertEquals(FIBONACCI, algorithmOfFib.run(randomNumberOfFib, "memoized"));
+    public void testFibonacciNumbersMemoized() throws Exception{
+        int f1 = algorithmOfFib.run(10, "naive");
+        Assert.assertEquals(f1, algorithmOfFib.run(10, "memoized"));
+        int f2 = algorithmOfFib.run(7, "recursive");
+        Assert.assertEquals(f2, algorithmOfFib.run(7, "memoized"));
+        int f3 = algorithmOfFib.run(19, "dynamic");
+        Assert.assertEquals(f3, algorithmOfFib.run(19, "dynamic"));
     }
     @Test
     public void testFibonacciNumbersDynamic() throws Exception{
